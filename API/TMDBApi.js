080d93864d48d493fb952ed896b83e461af748eb
Soilhat <soilhatmohamed@gmail.com>
2019-04-11 13:57:45 +0200

    Add Infinite Scroll

--- API/TMDBApi.js
+++ API/TMDBApi.js
@@ -1,8 +1,12 @@
 const API_TOKEN = "fda78fe66956fc02d6476a50ec8156cd";
 
-export function getFilms(text) {
-  const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text
+export function getFilms (text, page) {
+  const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text + "&page=" + page
   return fetch(url)
     .then((response) => response.json())
     .catch((error) => console.error(error))
+}
+
+export function getImage (name) {
+  return 'https://image.tmdb.org/t/p/w300' + name
 }
--- components/Search.js
+++ components/Search.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, View, TextInput, Button, Text, FlatList } from 'react-native'
+import { StyleSheet, View, TextInput, Button, Text, FlatList, ActivityIndicator } from 'react-native'
 import films from '../Helpers/filmsData'
 import FilmItem from './FilmItem'
 import {getFilms} from '../API/TMDBApi'
@@ -8,27 +8,72 @@ class Search extends React.Component {
 
   constructor(props) {
     super(props)
-    this._films = []
+    this.searchedText = ""
+    this.page = 0 //Compteur pour connaître la pae courante
+    this.totalPages= 0 // Nombres de pages pour savoir si on a atteint la fin des retours de l'API
+    this.state = {
+      films: [],
+      isLoading: false // Par défaut à false car il n'y a pas de chargement tant qu'on ne lance pas de recherche
+    }
   }
 
   _loadFilms() {
-    getFilms("star").then(data => {
-      console.log(data)
-      this._films = data.results
-      this.forceUpdate()
-    });
+    if (this.searchedText.length > 0) {
+      this.setState({ isLoading: true }) // Lancement du chargement
+      getFilms(this.searchedText, this.page+1).then(data => {
+        this.page = data.page
+        this.totalPages = data.total_pages
+          this.setState({
+            films: this.state.films.concat(data.results),
+            isLoading: false // Arrêt du chargement
+          })
+      })
+    }
   }
+
+  _inputTextChanged(text){
+    this.searchedText = text;
+    this.page = 0;
+    this.totalPages = 0
+    this.setState({
+      films: []
+    }, () => {
+      this._loadFilms();
+    })
+  }
+
+  _displayLoading() {
+      if (this.state.isLoading) {
+        return (
+          <View style={styles.loading_container}>
+            <ActivityIndicator size='large' />
+          </View>
+        )
+      }
+    }
 
   render() {
     return (
       <View style={styles.main_container}>
-        <TextInput style={ styles.textinput } placeholder='Titre du film'/>
-        <Button title='Rechercher' onPress={() => this._loadFilms()}/>
+        <TextInput
+          style={ styles.textinput }
+          placeholder='Titre du film'
+          onChangeText={(text) => this._inputTextChanged(text)}
+          /*onSubmitEditing={() => this._loadFilms()}*/
+        />
+        {/*<Button title='Rechercher' onPress={() => this._loadFilms()}/>*/}
         <FlatList
-          data={this._films}
+          data={this.state.films}
           keyExtractor={(item) => item.id.toString()}
           renderItem={({item}) => <FilmItem film={item}/>}
+          onEndReachedThreshold={0.5}
+          onEndReached={() => {
+            if(this.page < this.totalPages) {
+              this._loadFilms()
+            }
+          }}
         />
+        {this._displayLoading()}
       </View>
     )
   }
@@ -46,6 +91,15 @@ const styles = StyleSheet.create({
     borderColor: '#000000',
     borderWidth: 1,
     paddingLeft: 5
+  },
+  loading_container: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 100,
+    bottom: 0,
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 });
 
